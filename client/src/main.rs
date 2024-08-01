45a8e4ae7ada81d52a1b8173140b4033749e7b12
Himanshu-AT <aristech.official@gmail.com>
2024-08-02 04:35:15 +0530

    update client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,23 +1,42 @@
 use assets::Assets;
 use gpui::*;
-use settings::{default_settings, Settings, SettingsStore};
+use settings::Settings;
 use theme::{ThemeRegistry, ThemeSettings};
+use ui::prelude::*;
+use ui::Button;
 
-struct HelloWorld {
-    text: SharedString,
+// ignore unused variables
+#[allow(unused_variables)]
+
+struct CountNumber {
+    count: usize,
 }
 
-impl Render for HelloWorld {
+impl Render for CountNumber {
     fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
+        let button = Button::new("test-id", "click me")
+            .style(ui::ButtonStyle::Filled)
+            .size(ui::ButtonSize::Large)
+            .on_click(cx.listener(
+                move |this: &mut CountNumber,
+                      _selection: &ClickEvent,
+                      _cx: &mut ViewContext<CountNumber>| {
+                    this.count += 1;
+                },
+            ));
+
         div()
             .flex()
-            .bg(cx.theme().colors().background)
+            .flex_col()
+            .bg(rgb(0x282c34))
+            .text_color(rgb(0xffffff))
+            // .text(rgb(0xffffff))
             .size_full()
             .justify_center()
             .items_center()
             .text_xl()
-            .text_color(cx.theme().colors().text)
-            .child(format!("Hello, {}!", &self.text))
+            .child(format!("Pressed {}! times", &self.count))
+            .child(button)
     }
 }
 
@@ -25,25 +44,16 @@ fn main() {
     App::new().run(|cx: &mut AppContext| {
         let theme_name: String = "One Dark".to_string();
 
-        // let mut store = SettingsStore();
-        let mut store = SettingsStore::new(cx);
-        store
-            .set_default_settings(default_settings().as_ref(), cx)
-            .unwrap();
-        cx.set_global(store);
-
-        // settings::init(cx);
+        settings::init(cx);
         theme::init(theme::LoadThemes::All(Box::new(Assets)), cx);
 
-        let theme_registry: &ThemeRegistry = cx.global::<ThemeRegistry>();
-        let mut theme_settings: ThemeSettings = ThemeSettings::get_global(cx).clone();
+        let theme_registry = ThemeRegistry::global(cx);
+        let mut theme_settings = ThemeSettings::get_global(cx).clone();
         theme_settings.active_theme = theme_registry.get(&theme_name).unwrap();
         ThemeSettings::override_global(theme_settings, cx);
 
         cx.open_window(WindowOptions::default(), |cx| {
-            cx.new_view(|_cx| HelloWorld {
-                text: "World".into(),
-            })
+            cx.new_view(|_cx| CountNumber { count: 0 })
         })
         .unwrap();
     });
